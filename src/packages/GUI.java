706baae220f63246a90a34f01d7a130f3c7be2ca
Nour Eldeen Mohamed <119622675+NOURisLIGHT@users.noreply.github.com>
2024-12-08 14:25:39 +0200

    add GUI empty class

---
+++ src/packages/GUI.java
@@ -0,0 +1,14 @@
+package packages;
+
+import java.awt.Color;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import javax.swing.*;
+
+public class GUI {
+    
+}
